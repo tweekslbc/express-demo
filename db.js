c09bd883e6da963f24956e4a9319f8be53f9fc73
Tweeks <tweeks@daiwa.com>
2019-09-11 08:07:39 -0700

    video 7

--- db.js
+++ db.js
@@ -1,5 +1,17 @@
-const fs = require('fs');
+const fs = require("fs");
 const FILE = "./users.json";
+
+const writeJSON = data => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(FILE, JSON.stringify(data), (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    })
+  });
+};
 
 const readJSON = () => {
   return new Promise((resolve, reject) => {
@@ -17,10 +29,27 @@ const readJSON = () => {
   });
 };
 
+const createUser = user => {
+  return getUsers().then(users => {
+    const maxId = users.reduce((acc, user) => {
+      if (user.id > acc) {
+        acc = user.id;
+      }
+      return acc;
+    }, 0);
+    user.id = maxId + 1;
+    users.push(user);
+    return writeJSON(users);
+  })
+  .then(() => {
+    return user;
+  });
+};
 const getUsers = () => {
   return readJSON();
 };
 
 module.exports = {
-  getUsers
+  getUsers,
+  createUser
 };
