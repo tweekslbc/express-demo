863ee48a7a4b0f581e1561094f1ba46a20491266
Tweeks <tweeks@daiwa.com>
2019-09-11 07:43:46 -0700

    video 6

--- db.js
+++ db.js
@@ -1,10 +1,24 @@
-const users =[
-  { id: 1, name: 'moe'},
-  { id: 2, name: 'larry'}
-];
+const fs = require('fs');
+const FILE = "./users.json";
+
+const readJSON = () => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(FILE, (err, data) => {
+      if (data) {
+        try {
+          resolve(JSON.parse(data.toString()));
+        } catch (ex) {
+          reject(ex);
+        }
+      } else {
+        reject(err);
+      }
+    });
+  });
+};
 
 const getUsers = () => {
-  return users;
+  return readJSON();
 };
 
 module.exports = {
