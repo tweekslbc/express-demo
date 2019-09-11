3d85b29daf02d8026bdf82e0ef7cccfddc858f1e
Tweeks <tweeks@daiwa.com>
2019-09-10 20:10:54 -0700

    end of video 3

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 app.get('/',(req, res, next)=> {
-  res.send('hello world');
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 app.listen(3000, ()=> console.log('listening on port 3000'));
