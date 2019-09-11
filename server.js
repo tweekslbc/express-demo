9a661d9c3b221284da18744e84a3e2c2cb8d10b5
Tweeks <tweeks@daiwa.com>
2019-09-10 20:33:32 -0700

    display users

--- server.js
+++ server.js
@@ -5,4 +5,11 @@ const app = express();
 app.get('/',(req, res, next)=> {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
+app.get('/api/users', (req, res, next)=> {
+  const users =[
+    { id: 1, name: 'moe'},
+    { id: 2, name: 'larry'}
+  ];
+  res.send(users);
+});
 app.listen(3000, ()=> console.log('listening on port 3000'));
